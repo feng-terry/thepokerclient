15819651a0321f4b085ba643228a632878805a62
Angus <anguslin199@gmail.com>
2020-06-12 09:52:55 -0400

    player list div

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import {Route, Switch} from 'react-router-dom'
 
 function Form (){
     const [name,setName] = useState("")
---
+++ client/src/Components/Settings/PlayersList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlayersList(){
+    return(
+        <div className="playersListBox">
+            
+        </div>
+    )
+}
+
+export default PlayersList
